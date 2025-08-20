48a89de1734dd387babc6241a3540bb66126b42d
kalscium <kalscium@protonmail.com>
2025-08-21 09:04:58 +1000

    graphical editors: added a few packages
    Changes made:
      * graphical/apps/editors/default.nix:
        * (~) added krita, libresprite, blender, orca-slicer & freecad to
              packages
    
    Editors for my ambition.
    
    Signed-off-by: Ethan Ma <kalscium@protonmail.com>

--- graphical/apps/editors/default.nix
+++ graphical/apps/editors/default.nix
@@ -10,8 +10,12 @@
     obsidian
     # freecad
     wezterm
-    # krita # for drawing
-    pixelorama # for pixel art
+    krita # for drawing
+    libresprite # for pixel art
+    pixelorama # also, for pixel art
+    blender # for 3d modelling & cadding
+    freecad # perhaps some day I'll professionally cad
+    orca-slicer # for slicing 3d prints
     godot # for game development
     gimp
     libreoffice-fresh
